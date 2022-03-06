c9bee4f0f8700220b3f9160967a7c914db47be44
Guilherme Tonelotti <guilhermetonelotti33@gmail.com>
2022-03-06 11:15:59 -0300

    three commit

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const patch = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+    entry:['@babel/polyfill', path.resolve(__dirname, './src/main.js')],
+    output: {
+        path: path.resolve(__dirname, './public'),
+        filename: 'bundle.js'
+    },
+    devServer: {
+        static:{
+            directory: path.join(__dirname, './public'),
+        },
+        port:9000,
+    },
+     module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            __VUE_OPTIONS_API__: true,
+            __VUE_PROD_DEVTOOLS__: false,
+        }),
+    ],
+};
+    
+   
+
